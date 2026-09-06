6d716137b02f6f60e362484640d6cb67b4891331
JuanD <dosquisd123@gmail.com>
2026-09-06 16:53:45 -0500

    refactor: reorganize __init__.py by moving dynamic imports into __getattr__ and updating __all__ exports. Pulumi and jinja2 were required always when importing from utils, and sometimes aren't required any of those modules. That's the reason behind this change

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,21 +2,30 @@ from urllib.request import urlopen
 
 from src.utils.ansible import get_ansible_playbook_bin
 from src.utils.logger import logger
-from src.utils.pulumi import create_or_select_pulumi_stack
 from src.utils.randoms import get_random_generator, get_wireguard_port
-from src.utils.templates import render_inventory_template
 
-__all__ = [
-    "create_or_select_pulumi_stack",
-    "get_ansible_playbook_bin",
-    "get_public_ip",
-    "get_random_generator",
-    "get_wireguard_port",
-    "logger",
-    "render_inventory_template",
-]
+
+def __getattr__(name: str):
+    if name == "create_or_select_pulumi_stack":
+        from src.utils.pulumi import create_or_select_pulumi_stack
+
+        return create_or_select_pulumi_stack
+
+    if name == "render_inventory_template":
+        from src.utils.templates import render_inventory_template
+
+        return render_inventory_template
 
 
 def get_public_ip() -> str:
     with urlopen("https://api.ipify.org") as response:
         return response.read().decode("utf-8")
+
+
+__all__ = [
+    "get_ansible_playbook_bin",
+    "get_public_ip",
+    "get_random_generator",
+    "get_wireguard_port",
+    "logger",
+]
