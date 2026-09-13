25ae2e44ff6ff552733cccc51e9a59f26dd117b2
JuanD <dosquisd123@gmail.com>
2026-09-13 16:13:01 -0500

    style: formating files

--- src/wisp/config/constants.py
+++ src/wisp/config/constants.py
@@ -17,6 +17,7 @@ def __project_root(anchor: str = "pyproject.toml"):
         f"Could not find {anchor} in the parent directories of {path}"
     )
 
+
 CREATED_BY_TAG: str = "wisp"
 PULUMI_STACK_NAME: str = "wisp-stack"
 PULUMI_PROJECT_NAME: str = "wisp-project"
